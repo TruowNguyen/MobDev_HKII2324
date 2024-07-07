b09221a47ac0f31ff29aedbe3d8f79934db1ac5a
TruowNguyen <nguyentruonghk17@gmail.com>
2024-07-07 19:21:47 +0700

    ok

--- Unit1/Lesson 2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit1/Lesson 2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.example.lemonade
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +46,156 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                    ) {
+                    LemonadeApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                val title = @androidx.compose.runtime.Composable {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when(currentStep) {
+                1 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_tree),
+                            contentDescription = stringResource(R.string.lemon_tree_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 2 }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.lemon_select)
+                        )
+                    }
+                }
+                2 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_squeeze),
+                            contentDescription = stringResource(R.string.lemon_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 3 }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.lemon_squeeze)
+                        )
+                    }
+                }
+                3 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_drink),
+                            contentDescription = stringResource(R.string.lemonade_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 4 }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.lemon_drink)
+                        )
+                    }
+                }
+                4 -> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_restart),
+                            contentDescription = stringResource(R.string.empty_glass_description),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable { currentStep = 1 }
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(text = stringResource(R.string.lemon_empty_glass)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
 }
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawbleResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+    ) {
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(
+                onClick = onImageClick,
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+            ) {
+                Image(
+                    painter = painterResource(drawbleResourceId),
+                    contentDescription = stringResource(contentDescriptionResourceId),
+                    modifier = Modifier
+                )
+            }
+            Text(
+                text = stringResource(textLabelResourceId),
+                style = MaterialTheme.typography.bodyLarge
+                )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
